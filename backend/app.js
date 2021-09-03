dc941777bf26168441dff850d58dd8cb2260ca4c
Charly M <35889038+ACJF00@users.noreply.github.com>
2021-09-03 14:54:19 +0200

    app

--- backend/app.js
+++ backend/app.js
@@ -10,10 +10,10 @@ const userRoutes = require('./routes/user');
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+  next()
 });
 
 mongoose.connect('mongodb+srv://Charly:muziotti@go-fullstack.74vdo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
