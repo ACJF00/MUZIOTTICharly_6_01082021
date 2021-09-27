e6aa94b7e6b43bdd0987a57c625f1f24fb80c69e
Charly M <35889038+ACJF00@users.noreply.github.com>
2021-09-27 16:12:09 +0200

    Final commit

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const app = express();
+
+// cryptage email //
+require('dotenv').config()
+
+// avoid SQL injections //
+const mongoSanitize = require('express-mongo-sanitize');
+
+// avoid XSS injection //
+const helmet = require("helmet");
+app.use(helmet());
+
+// Rate limiting //
+const rateLimit = require("express-rate-limit");
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 1000, // limit each IP to 10 requests per windowMs
+  message: "Trop de requêtes, veuillez réessayer après 15 minutes."
+});
+app.use(limiter);
 
 const sauceRoutes = require('./routes/sauce')
 
 const userRoutes = require('./routes/user');
-
-const app = express();
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
@@ -16,7 +34,8 @@ app.use((req, res, next) => {
   next()
 });
 
-mongoose.connect('mongodb+srv://Charly:muziotti@go-fullstack.74vdo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+const mongooseConnect= `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@go-fullstack.74vdo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+mongoose.connect(mongooseConnect,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -26,7 +45,7 @@ app.use(bodyParser.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use('/api/sauce', sauceRoutes)
+app.use('/api/sauces', sauceRoutes)
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,80 +1,186 @@
-const Sauce = require('../models/sauce');
+const Sauce = require('../models/sauce')
+const fs = require('fs')
 
 exports.createSauce = (req, res, next) => {
-  const sauceObject = JSON.parse(req.body.sauce);
-  delete sauceObject._id;
+  const sauceObject = JSON.parse(req.body.sauce)
+  delete sauceObject._id
   const sauce = new Sauce({
-    ...saucebject,
+    ...sauceObject,
     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-  });
+  })
   sauce.save()
     .then(() => {
       res.status(201).json({
         message: 'Post saved successfully!'
-      });
+      })
     }
   ).catch(
     (error) => {
       res.status(400).json({
         error: error
-      });
+      })
     }
-  );
-};
+  )
+}
 
 exports.getOneSauce = (req, res, next) => {
   Sauce.findOne({
     _id: req.params.id
   }).then(
     (sauce) => {
-      res.status(200).json(sauce);
+      res.status(200).json(sauce)
     }
   ).catch(
     (error) => {
       res.status(404).json({
         error: error
-      });
+      })
     }
-  );
-};
+  )
+}
 
 exports.modifySauce = (req, res, next) => {
   const sauceObject = req.file ?
     {
       ...JSON.parse(req.body.sauce),
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : { ...req.body };
+    } : { ...req.body }
   Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
     .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-};
+    .catch(error => res.status(400).json({ error }))
+}
 
 exports.deleteSauce = (req, res, next) => {
-  Sauce.deleteOne({_id: req.params.id}).then(
-    () => {
-      res.status(200).json({
-        message: 'Deleted!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-};
+  Sauce.findOne({ _id: req.params.id })
+    .then(sauce => {
+      const filename = sauce.imageUrl.split('/images/')[1]
+      fs.unlink(`images/${filename}`, () => {
+        Sauce.deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+          .catch(error => res.status(400).json({ error }))
+      })
+    })
+    .catch(error => res.status(500).json({ error }))
+}
 
 exports.getAllSauce = (req, res, next) => {
   Sauce.find().then(
     (sauces) => {
-      res.status(200).json(sauces);
+      res.status(200).json(sauces)
     }
   ).catch(
     (error) => {
       res.status(400).json({
         error: error
-      });
+      })
     }
-  );
-};
+  )
+}
+
+exports.rateSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+  .then((sauce) => {
+    if (req.body.like === 1 && !sauce.usersLiked.includes(req.body.userId)){
+      sauce.usersLiked.push(req.body.userId)
+      sauce.likes++
+      sauce.save()
+      res.status(201).json({ message: 'Your like has been added !' })
+    }
+
+    if (req.body.like === 1 && sauce.usersLiked.includes(req.body.userId)){
+      res.status(403).json({ message: "You can't like the same sauce twice"})
+    }
+  
+    if (req.body.like === -1 && !sauce.usersDisliked.includes(req.body.userId)){
+      sauce.usersDisliked.push(req.body.userId)
+      sauce.dislikes++
+      sauce.save()
+      res.status(201).json({ message: 'Your dislike has been added !' })
+    }
+
+    if (req.body.like === -1 && sauce.usersDisliked.includes(req.body.userId)){
+      res.status(403).json({ message: "You can't dislike the same sauce twice"})
+    }
+
+    if (req.body.like === 0) {
+      if (sauce.usersLiked.includes(req.body.userId)){
+        sauce.usersLiked.pull(req.body.userId)
+        sauce.likes--
+        sauce.save()
+        res.status(201).json({ message: 'Your like has been added !' })
+      } else if (sauce.usersDisliked.includes(req.body.userId)) {
+        sauce.usersDisliked.pull(req.body.userId)
+        sauce.dislikes--
+        sauce.save()
+        res.status(201).json({ message: 'Your like has been added !' })
+      } else {
+        res.status(403).json({ message: "You didn't interact with the sauce yet"})
+      }
+    }
+  })
+}
+
+
+
+/** 
+exports.rateSauce = (req, res, next) => {
+  //check if there's a like in the body
+  switch (req.body.like) {
+    // in case there is :
+    case 0:
+      Sauce.findOne({ _id: req.params.id })
+        .then((sauce) => {
+          // if user already liked
+          if (sauce.usersLiked.find(user => user === req.body.userId)) {
+            // is the user in the usersLiked array
+            Sauce.updateOne({ _id: req.params.id }, {
+              // decrease likes by 1 and remove user from usersLiked
+              $inc: { likes: -1 },
+              $pull: { usersLiked: req.body.userId },
+            })
+              .then(() => res.status(200).json({ message: 'Your like has been removed !' }))
+              .catch(error => res.status(400).json({ error }))
+          }
+
+          // if user already disliked
+          if (sauce.usersDisliked.find(user => user === req.body.userId)) {
+            // check if user in the usersDisliked array
+            Sauce.updateOne({ _id: req.params.id }, {
+              // decrease dislikes by 1 and remove user from usersDisliked
+              $inc: { dislikes: -1 },
+              $pull: { usersDisliked: req.body.userId },
+            })
+              .then(() => res.status(200).json({ message: 'Your dislike has been removed !' }))
+              .catch(error => res.status(400).json({ error }))
+          }
+        })
+        .catch(error => res.status(404).json({ error }))
+      break
+
+    // add like
+    case 1:
+      Sauce.updateOne({ _id: req.params.id }, {
+        // increase likes by 1 and add user to usersLiked
+        $inc: { likes: 1 },
+        $push: { usersLiked: req.body.userId },
+      })
+        .then(() => res.status(201).json({ message: 'Your like has been added !' }))
+        .catch(error => res.status(400).json({ error }))
+      break
+
+    // add dislike
+    case -1:
+      Sauce.updateOne({ _id: req.params.id }, {
+        // increase dislikes by 1 and add user to usersDisliked
+        $inc: { dislikes: 1 },
+        $push: { usersDisliked: req.body.userId },
+      })
+        .then(() => res.status(201).json({ message: 'Your dislike has been added !' }))
+        .catch(error => res.status(400).json({ error }))
+      break
+
+    default:
+      console.error('Bad request !')
+  }
+}
+*/
